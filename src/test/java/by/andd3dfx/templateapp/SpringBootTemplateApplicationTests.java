b0ce1d9feec5056b9f9381ad7e0961cb447fa44c
Andrei Punko <Andrei_Punko@epam.com>
2020-05-27 15:49:37 +0300

    Add project using template from https://github.com/andrei-punko/spring-boot-template

---
+++ src/test/java/by/andd3dfx/templateapp/SpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package by.andd3dfx.templateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
