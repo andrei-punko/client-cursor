0d30517122cf20be5718022231c9a358ff64eea0
Andrei Punko <Andrei_Punko@epam.com>
2020-05-28 17:38:46 +0300

    Adjust exceptions

--- src/main/java/by/andd3dfx/templateapp/exceptions/ArticleNotFoundException.java
+++ src/main/java/by/andd3dfx/templateapp/exceptions/ArticleNotFoundException.java
@@ -1,5 +1,9 @@
 package by.andd3dfx.templateapp.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ArticleNotFoundException extends NotFoundException {
 
     public ArticleNotFoundException(Long id) {
--- src/main/java/by/andd3dfx/templateapp/exceptions/NotFoundException.java
+++ src/main/java/by/andd3dfx/templateapp/exceptions/NotFoundException.java
@@ -1,9 +1,5 @@
 package by.andd3dfx.templateapp.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public abstract class NotFoundException extends RuntimeException {
 
     public NotFoundException(String name, Long id) {
